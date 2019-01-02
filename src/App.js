6e5c488c5683c9e8d485528aeac36d81fb279bd9
ArcticPheenix <phoenix.ckelly@gmail.com>
2019-01-02 16:02:12 -0700

    Basic CSS updates and layout.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
 import PriceTable from './PriceTable';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <PriceTable />
+        <Footer />
       </div>
     );
   }
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div className='App-footer'>
+            <p>Copyright 2019</p>
+        </div>
+    );
+};

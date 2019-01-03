4c30c2001c413703d2d46c40e99f4b47338ee761
ArcticPheenix <phoenix.ckelly@gmail.com>
2019-01-02 20:43:11 -0700

    Added more commodity data for testing.

--- src/commodity_data.js
+++ src/commodity_data.js
@@ -276,4 +276,280 @@ export default
         buyPrice: 24.00,
         sellPrice: 0.0
     },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Agricium',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Agricultural Supplies',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Altruciatoxin',
+        buyPrice: 11.80,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Astatine',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Beryl',
+        buyPrice: 0.0,
+        sellPrice: 4.21
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Chlorine',
+        buyPrice: 0.0,
+        sellPrice: 1.44
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Corundum',
+        buyPrice: 0.0,
+        sellPrice: 2.41
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Diamond',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Distilled Spirits',
+        buyPrice: 4.95,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Flourine',
+        buyPrice: 0.0,
+        sellPrice: 2.63
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Gold',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Hydrogen',
+        buyPrice: 0.0,
+        sellPrice: 0.96
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Iodine',
+        buyPrice: 0.0,
+        sellPrice: 0.34
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Laranite',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Medical Supplies',
+        buyPrice: 17.72,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Processed Food',
+        buyPrice: 1.36,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Quartz',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Scrap',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Stims',
+        buyPrice: 3.36,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Titanium',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Tungsten',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'Waste',
+        buyPrice: 0.0,
+        sellPrice: 0.01
+    },
+    {
+        location: '[Yela] Benson Mining Outpost',
+        commodity: 'WiDoW',
+        buyPrice: 24.00,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Agricium',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Agricultural Supplies',
+        buyPrice: 1.13,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Altruciatoxin',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Astatine',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Beryl',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Chlorine',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Corundum',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Diamond',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Distilled Spirits',
+        buyPrice: 0.0,
+        sellPrice: 4.65
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Flourine',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Gold',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Hydrogen',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Iodine',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Laranite',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Medical Supplies',
+        buyPrice: 18.05,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Processed Food',
+        buyPrice: 0.0,
+        sellPrice: 1.34
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Quartz',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Scrap',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Stims',
+        buyPrice: 0.0,
+        sellPrice: 3.10
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Titanium',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Tungsten',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'Waste',
+        buyPrice: 0.0,
+        sellPrice: 0.01
+    },
+    {
+        location: '[Daymar] Bountiful Harvest Hydroponics',
+        commodity: 'WiDoW',
+        buyPrice: 24.00,
+        sellPrice: 0.0
+    },
   ];

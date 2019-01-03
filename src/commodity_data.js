0561f05dc21478e96aec26e0b642eb53d7d089ea
ArcticPheenix <phoenix.ckelly@gmail.com>
2019-01-02 19:27:48 -0700

    WIP: Commodity data added

--- src/commodity_data.js
+++ src/commodity_data.js
@@ -1,27 +1,279 @@
 export default
 [
     {
-      location: '[Daymar] ArcCorp Mining Area 141',
-      commodity: 'Agricium',
-      buyPrice: 0.0,
-      sellPrice: 24.45
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Agricium',
+        buyPrice: 0.0,
+        sellPrice: 24.45
     },
     {
-      location: '[Daymar] ArcCorp Mining Area 141',
-      commodity: 'Altruciatoxin',
-      buyPrice: 11.80,
-      sellPrice: 0.0
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Agricultural Supplies',
+        buyPrice: 0.0,
+        sellPrice: 0.0
     },
     {
-      location: '[Yela] ArcCorp Mining Area 157',
-      commodity: 'Altruciatoxin',
-      buyPrice: 11.81,
-      sellPrice: 0.0
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Altruciatoxin',
+        buyPrice: 11.80,
+        sellPrice: 0.0
     },
     {
-      location: '[Yela] ArcCorp Mining Area 157',
-      commodity: 'Chlorine',
-      buyPrice: 0.0,
-      sellPrice: 1.44
-    }
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Astatine',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Beryl',
+        buyPrice: 0.0,
+        sellPrice: 4.21
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Chlorine',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Corundum',
+        buyPrice: 0.0,
+        sellPrice: 2.41
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Diamond',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Distilled Spirits',
+        buyPrice: 4.88,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Flourine',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Gold',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Hydrogen',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Iodine',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Laranite',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Medical Supplies',
+        buyPrice: 18.03,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Processed Food',
+        buyPrice: 1.36,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Quartz',
+        buyPrice: 0.0,
+        sellPrice: 1.35
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Scrap',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Stims',
+        buyPrice: 3.40,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Titanium',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Tungsten',
+        buyPrice: 0.0,
+        sellPrice: 3.73
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'Waste',
+        buyPrice: 0.0,
+        sellPrice: 0.01
+    },
+    {
+        location: '[Daymar] ArcCorp Mining Area 141',
+        commodity: 'WiDoW',
+        buyPrice: 24.00,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Agricium',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Agricultural Supplies',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Altruciatoxin',
+        buyPrice: 11.81,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Astatine',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Beryl',
+        buyPrice: 0.0,
+        sellPrice: 4.21
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Chlorine',
+        buyPrice: 0.0,
+        sellPrice: 1.44
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Corundum',
+        buyPrice: 0.0,
+        sellPrice: 2.41
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Diamond',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Distilled Spirits',
+        buyPrice: 4.95,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Flourine',
+        buyPrice: 0.0,
+        sellPrice: 2.63
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Gold',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Hydrogen',
+        buyPrice: 0.0,
+        sellPrice: 0.98
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Iodine',
+        buyPrice: 0.0,
+        sellPrice: 0.34
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Laranite',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Medical Supplies',
+        buyPrice: 18.05,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Processed Food',
+        buyPrice: 1.36,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Quartz',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Scrap',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Stims',
+        buyPrice: 3.40,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Titanium',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Tungsten',
+        buyPrice: 0.0,
+        sellPrice: 0.0
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'Waste',
+        buyPrice: 0.0,
+        sellPrice: 0.01
+    },
+    {
+        location: '[Yela] ArcCorp Mining Area 157',
+        commodity: 'WiDoW',
+        buyPrice: 24.00,
+        sellPrice: 0.0
+    },
   ];

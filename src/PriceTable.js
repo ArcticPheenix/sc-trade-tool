2c4dd6fd677be1f3ca12e15e0d7397603261885a
ArcticPheenix <phoenix.ckelly@gmail.com>
2019-01-04 17:49:42 -0700

    Adding support for SQLite3

--- src/PriceTable.js
+++ src/PriceTable.js
@@ -2,16 +2,35 @@ import React from 'react';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 import testData from './commodity_data';
+import sqlite3 from 'sqlite3';
+// import fs from 'fs';
 
 export default class PriceTable extends React.Component {
   constructor(props) {
     super(props);
     this.data = this.data.bind(this);
     this.columns = this.columns.bind(this);
+    this.sqlsetup = this.sqlsetup.bind(this);
+  }
+
+  sqlsetup(file) {
+    // if (file === "") {
+    //   console.log("[ERROR] No database file specified!");
+    //   return;
+    // }
+    const db = new sqlite3.Database(file);
+    const stmt = db.prepare('SELECT * FROM v_prices');
+    const rows = stmt.all((err, rows) => {
+      rows.forEach(row => {
+        console.log(row);
+      });
+    });
   }
 
   data() {
     // TODO - Basic data gathering function
+    // fs.open('database/scdata.db')
+    this.sqlsetup('database/scdata.db');
     const dataArray = testData;
     return dataArray;
   }

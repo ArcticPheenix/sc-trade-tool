8fb7907f951ebb663636fb88e1b998713bbd4a6a
ArcticPheenix <phoenix.ckelly@gmail.com>
2019-01-21 15:43:19 -0700

    Experimenting with a different data table library.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
-import PriceTable from './PriceTable';
+// import PriceTable from './PriceTable';
+import SmartDataTable from 'react-smart-data-table';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <PriceTable />
+        {/* <PriceTable /> */}
+        <SmartDataTable
+          data='http://localhost:4000/v1/pricetable'
+          dataKey='data'
+          name='test-table'
+          className='ui compact selectable table'
+          perPage={20}
+          sortable
+        />
         <Footer />
       </div>
     );
